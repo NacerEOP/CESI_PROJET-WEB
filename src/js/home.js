4f29e299e0e5b72be91d1b546d11fefd04a641d2
Nacer <nacer.benk72@gmail.com>
2026-02-08 21:00:10 +0100

    class slideshow

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,11 @@
 import { ImgSlideShow } from "./imageSlideShow.js";
 
 new ImgSlideShow(
-  document.getElementById("home-Header"),
+  document.getElementById("home-header"),
   [
     "../assets/images/internship1.png",
     "../assets/images/internship2.jpg",
     "../assets/images/internship3.jpg"
   ],
-  4000
+  10000
 ).start();

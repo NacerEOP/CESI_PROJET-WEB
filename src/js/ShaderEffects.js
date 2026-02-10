26a9977faa80e8dba731554e9a9bbcd4f16c9917
NacerEOP <nacer.benk72@gmail.com>
2026-02-10 16:55:38 +0100

    a bunch of shading

--- src/js/ShaderEffects.js
+++ src/js/ShaderEffects.js
@@ -2,12 +2,14 @@ import { ShaderProgram } from "./ShaderProgram.js";
 
 export class CurvedTrackEffect {
   async init(gl, app) {
+    this.gl = gl;
+    this.app = app;
+
     this.program = new ShaderProgram(
       gl,
       "../shaders/curved.vert",
       "../shaders/curved.frag"
     );
-
     await this.program.load();
 
     // geometry (one quad)
@@ -16,7 +18,7 @@ export class CurvedTrackEffect {
     gl.bufferData(
       gl.ARRAY_BUFFER,
       new Float32Array([
-        -0.5, -0.5,
+        -0.5, -0.9,
          0.5, -0.5,
         -0.5,  0.9,
          0.5,  0.5,
@@ -24,17 +26,33 @@ export class CurvedTrackEffect {
       gl.STATIC_DRAW
     );
 
+    // IMPORTANT: use program BEFORE querying locations / setting uniforms
+    this.program.use();
+
+    // attribs
     this.posLoc = gl.getAttribLocation(this.program.program, "a_pos");
+
+    // uniforms (must exist in your .frag)
     this.timeLoc = gl.getUniformLocation(this.program.program, "u_time");
+    this.resLoc  = gl.getUniformLocation(this.program.program, "u_resolution");
+
+    // set initial resolution once (also do this again if canvas size changes)
+    const canvas = gl.canvas;
+    gl.uniform2f(this.resLoc, canvas.width, canvas.height);
   }
 
   render(gl, app, t) {
     this.program.use();
 
+    // update resolution every frame (safe + handles resizes)
+    const canvas = gl.canvas;
+    gl.uniform2f(this.resLoc, canvas.width, canvas.height);
+
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
     gl.enableVertexAttribArray(this.posLoc);
     gl.vertexAttribPointer(this.posLoc, 2, gl.FLOAT, false, 0, 0);
 
+    // time in seconds
     gl.uniform1f(this.timeLoc, t * 0.001);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);

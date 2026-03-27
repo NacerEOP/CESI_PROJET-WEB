200345c2bf5484480c0ecb7cc63578cf9b444f5c
leith-allah <leithallah.contact@gmail.com>
2026-03-27 17:15:53 +0100

    DashboardController.php

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controllers;
 
+use App\Models\Auth;
+
 class DashboardController extends BaseController
 {
     public function index()
     {
+        if (!Auth::isLoggedIn()) {
+            header('Location: /NEWMVCtwigArchitecture/login');
+            exit;
+        }
+
         $this->render('dashboard', [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'user' => Auth::user(),
         ]);
     }
 }
